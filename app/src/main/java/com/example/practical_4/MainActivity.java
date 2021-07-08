05b2a1faf136d365be42a89e7108c79e7ec511f7
zileong <ongzileong@gmail.com>
2021-07-08 18:29:13 +0800

    Question 3

--- app/src/main/java/com/example/practical_4/MainActivity.java
+++ app/src/main/java/com/example/practical_4/MainActivity.java
@@ -7,11 +7,15 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.graphics.Typeface;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.ArcShape;
+import android.graphics.drawable.shapes.RectShape;
+import android.graphics.drawable.shapes.RoundRectShape;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,31 +24,44 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new CustomView(this));
+        /**
+         * Question 3: Draw an Inner Rounded Rectangle and a normal Rectangle
+         * Comment out to see 2 difference code
+         */
+        ShapeDrawable rect = new ShapeDrawable(new RectShape());
+        rect.getPaint().setColor(Color.YELLOW);
+        rect.getPaint().setStyle(Paint.Style.FILL_AND_STROKE);
+
+        // rounded rectangle
+        float [] outerRadii = new float[]{5, 5, 5, 5, 5, 5, 5, 5}; // outer radius value
+        float [] innerRadii = new float[]{5, 5, 5, 5, 5, 5, 5, 5}; // inner radius value
+        RectF insetRectangle = new RectF(10, 10, 10, 10);
+
+        ShapeDrawable roundedRect = new ShapeDrawable(new RoundRectShape(outerRadii, insetRectangle, innerRadii));
+        roundedRect.getPaint().setColor(Color.RED);
+
+        // aspect ratio for width and height
+        rect.setIntrinsicHeight(1);
+        rect.setIntrinsicWidth(2);
+        roundedRect.setIntrinsicHeight(50);
+        roundedRect.setIntrinsicWidth(100);
+
+        // Create ImageView to show Rectangle
+        ImageView imageView = new ImageView(this);
+        imageView.setImageDrawable(rect);
+        imageView.setImageDrawable(roundedRect);
+
+        // Set content View to ImageView
+        setContentView(imageView);
     }
 
     public static class CustomView extends View {
-
-        private ShapeDrawable drawable;
-
         public CustomView(Context context) {
             super(context);
-
-            // Method 1: Draw shape on CustomView
-            int x = 100;
-            int y = 100;
-            int w = 600;
-            int h = 100;
-
-            drawable = new ShapeDrawable(new ArcShape(0, 360));
-            drawable.getPaint().setColor(0xff74AC23);
-            drawable.setBounds(x, y, x+w, y+h);
         }
 
         @Override
         protected void onDraw(Canvas canvas) {
-            drawable.draw(canvas);
-
         }
     }
 }
