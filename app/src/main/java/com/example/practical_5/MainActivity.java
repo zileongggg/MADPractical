2fe54148e9804c47711a14f05be66bc20d523981
zileong <ongzileong@gmail.com>
2021-07-13 14:49:54 +0800

    First commit

---
+++ app/src/main/java/com/example/practical_5/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.practical_5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
