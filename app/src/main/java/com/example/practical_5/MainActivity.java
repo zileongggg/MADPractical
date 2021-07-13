7df2559429116fa08674fff8395b5680c68b82a6
zileong <ongzileong@gmail.com>
2021-07-13 14:52:33 +0800

    Question 1

--- app/src/main/java/com/example/practical_5/MainActivity.java
+++ app/src/main/java/com/example/practical_5/MainActivity.java
@@ -8,9 +8,37 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SQLiteAdapter mySQLiteAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        TextView listContent = new TextView(this);
+        LinearLayout ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+        listContent.setTextSize(20f);
+
+        mySQLiteAdapter = new SQLiteAdapter(this);
+
+        // open to write
+        mySQLiteAdapter.openToWrite();
+        mySQLiteAdapter.deleteAll();
+        mySQLiteAdapter.insert("Burger");
+        mySQLiteAdapter.insert("Nasi Lemak");
+        mySQLiteAdapter.insert("Ayam Goreng");
+        mySQLiteAdapter.insert("Curry Laksa");
+        mySQLiteAdapter.insert("KFC");
+        mySQLiteAdapter.close();
+
+        // open to read
+        mySQLiteAdapter.openToRead();
+        String contentRead = mySQLiteAdapter.queueAll();
+        mySQLiteAdapter.close();
+
+        listContent.setText(contentRead);
+        ll.addView(listContent);
+        setContentView(ll);
 
     }
 }
---
+++ app/src/main/java/com/example/practical_5/SQLiteAdapter.java
@@ -0,0 +1,81 @@
+package com.example.practical_5;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+
+public class SQLiteAdapter {
+    public static final String MYDATABASE_NAME = "MY_DATABASE";
+    public static final String MYDATABASE_TABLE = "MY_TABLE";
+    public static final int MYDATABASE_VERSION = 1;
+    public static final String KEY_CONTENT = "Content";
+    // create table MY_TABLE (Content text not null);
+    private static final String SCRIPT_CREATE_DATABASE = "create table "
+            + MYDATABASE_TABLE + " (" + KEY_CONTENT + " text not null);";
+    private SQLiteHelper sqLiteHelper;
+    private SQLiteDatabase sqLiteDatabase;
+    private Context context;
+
+    public SQLiteAdapter(Context c) {
+        context = c;
+    }
+
+    public SQLiteAdapter openToRead() throws android.database.SQLException {
+        sqLiteHelper = new SQLiteHelper(context, MYDATABASE_NAME, null, MYDATABASE_VERSION);
+        sqLiteDatabase = sqLiteHelper.getReadableDatabase();
+        return this;
+    }
+
+    public SQLiteAdapter openToWrite() throws android.database.SQLException {
+        sqLiteHelper = new SQLiteHelper(context, MYDATABASE_NAME, null, MYDATABASE_VERSION);
+        sqLiteDatabase = sqLiteHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close() {
+        sqLiteHelper.close();
+    }
+
+    public long insert(String content) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(KEY_CONTENT, content);
+        return sqLiteDatabase.insert(MYDATABASE_TABLE, null, contentValues);
+    }
+
+    public int deleteAll() {
+        return sqLiteDatabase.delete(MYDATABASE_TABLE, null, null);
+    }
+
+    // Question 1
+    public String queueAll() {
+        String[] columns = new String[]{KEY_CONTENT};
+        Cursor cursor = sqLiteDatabase.query(MYDATABASE_TABLE, columns,
+                null, null, null, null, null);
+
+        String result = "";
+        int index_CONTENT = cursor.getColumnIndex(KEY_CONTENT);
+
+        for (cursor.moveToFirst(); !(cursor.isAfterLast()); cursor.moveToNext()) {
+            result = result + cursor.getString(index_CONTENT) + "\n";
+        }
+        return result;
+    }
+
+    public class SQLiteHelper extends SQLiteOpenHelper {
+        public SQLiteHelper(Context context, String name, CursorFactory factory, int version) {
+            super(context, name, factory, version);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SCRIPT_CREATE_DATABASE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        }
+    }
+}
